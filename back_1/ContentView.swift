20ccc52d103856fd017bfbb4edf848e220fcdd2a
Cheol-Jin <cil6879@gmail.com>
2023-04-25 19:07:35 +0900

    Initial Commit

---
+++ back_1/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  back_1
+//
+//  Created by 최철진 on 2023/04/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ back_1/back_1App.swift
@@ -0,0 +1,17 @@
+//
+//  back_1App.swift
+//  back_1
+//
+//  Created by 최철진 on 2023/04/25.
+//
+
+import SwiftUI
+
+@main
+struct back_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
